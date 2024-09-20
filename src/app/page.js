b1d9de7594e996e283fd92ff5e04b3fc00c7ca68
trevornei <117675657+trevornei@users.noreply.github.com>
2024-09-20 12:52:29 -0600

    Changed font size.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
 	<div className="flex flex-col items-center justify-center">
-	  <h1>
+	  <h1 className="text-3xl">
 	    Page Under Dev(struction)elopment.
 	  </h1>
 	  <progress className="progress w-56"></progress>
