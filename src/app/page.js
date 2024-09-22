f89157777d010a5c4b10923e879ad28f7c081b2c
trevornei <117675657+trevornei@users.noreply.github.com>
2024-09-21 23:08:04 -0600

    change

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
 export default function Home() {
   return (
-	<div className="flex flex-col items-center justify-center">
-	  <h1 className="text-3xl">
-	    Page Under Dev(struction)elopment.
-	  </h1>
-	  <progress className="progress w-56"></progress>
-	</div>
-  );
+    <>
+	  <div className="">
+		
+	  </div>
+	</> 
+  )
 }
