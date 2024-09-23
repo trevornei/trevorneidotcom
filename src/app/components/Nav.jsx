d54a945c85f968477c68525830b554d3a6e3a502
trevornei <117675657+trevornei@users.noreply.github.com>
2024-09-23 15:17:24 -0600

    A lot wowo

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,22 +1,32 @@
-'use client'
+"use client";
 import React from "react";
+import Image from "next/image";
 
 export default function Nav() {
   return (
     <>
-      <nav className="flex flex-row border-2 border-solid border-error navbar bg-bgdg">
-        <div className=""></div>
-        <ul className="">
-          <li className="">
-            <h1 className="text-tp">Blog</h1>
-          </li>
-          <li className="">
-            <h1 className="text-tp">About</h1>
-          </li>
-          <li className="">
-            <h1 className="text-tp">Contact</h1>
-          </li>
-        </ul>
+      <nav className="flex flex-row items-center justify-between border-2 border-solid xl:px-10 border-error navbar bg-bgdg">
+        <div className="">
+          <Image
+            src="/CirclesAndSpheres/SVG/circles_40.svg"
+            width={80}
+            height={80}
+            alt="Logo"
+          />
+        </div>
+        <div className="">
+          <ul className="flex flex-row gap-10">
+            <li className="">
+              <h1 className="text-tp">Blog</h1>
+            </li>
+            <li className="">
+              <h1 className="text-tp">About</h1>
+            </li>
+            <li className="">
+              <h1 className="text-tp">Contact</h1>
+            </li>
+          </ul>
+        </div>
       </nav>
     </>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Nav from "./components/Nav";
+import Image from "next/image";
 import Footer from "./components/Footer";
 
 export default function Home() {
@@ -6,6 +7,32 @@ export default function Home() {
     <>
       <div className="flex flex-col">
         <Nav />
+        <div className="flex flex-row bg-tp/50 ">
+          <div className="flex flex-row items-center justify-center backdrop-blur-x">
+            <Image
+              src="/CirclesAndSpheres/SVG/circles_35.svg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </div>
+          <div className="flex flex-row items-center justify-center ">
+            <Image
+              src="/CirclesAndSpheres/SVG/circles_26.svg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </div>
+          <div className="flex flex-row items-center justify-center ">
+            <Image
+              src="/CirclesAndSpheres/SVG/circles_57.svg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </div>
+        </div>
         <Footer />
       </div>
     </>
