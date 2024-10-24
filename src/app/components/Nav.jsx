f43bab7d1961bc2e0b77e871e73e09e92834fbd8
trevornei <117675657+trevornei@users.noreply.github.com>
2024-10-24 14:59:30 -0600

    test

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -7,6 +7,8 @@ import { useGsap } from "gsap/dist/ScrollTrigger";
 export default function Nav() {
   const logoRef = useRef(null); // Reference for the logo
 
+  const navUt = useRef(null); // Reference for the logo
+  
   useEffect(() => {
     const logoElement = logoRef.current;
 
@@ -33,9 +35,35 @@ export default function Nav() {
     };
   }, []);
 
+  useEffect(() => {
+    const navBtn = navUt.current;
+
+    const rotateLogo = (e) => {
+      const rect = navBtn.getBoundingClientRect();
+      const logoCenterX = rect.left + rect.width / 2;
+      const logoCenterY = rect.top + rect.height / 2;
+
+      // Calculate the angle based on mouse position relative to the logo
+      const angle =
+        Math.atan2(e.clientY - logoCenterY, e.clientX - logoCenterX) *
+        (180 / Math.PI);
+
+      // Apply the rotation using GSAP
+      gsap.to(navBtn, { rotation: angle, duration: 0.5, ease: "power3.out" });
+    };
+
+    // Add the mousemove event listener
+    window.addEventListener("mousemove", rotateLogo);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("mousemove", rotateLogo);
+    };
+  }, []);
+
   return (
     <>
-      <nav className="z-50 flex flex-row items-center justify-between xl:px-10 navbar bg-bgdg">
+      <nav className="z-50 flex sticky flex-row items-center justify-between xl:px-10 navbar bg-gradient-to-b from-ts/30 to-bgdg">
         <div className="">
           <Image
             src="/assets/images/CirclesAndSpheres/SVG/circles_79_white.svg"
@@ -46,12 +74,12 @@ export default function Nav() {
             ref={logoRef} // Set the ref to target the logo
           />
         </div>
-        <div>  
-          <button>
-           <div className="w-8 h-8 rounded-full bg-tp flex items-center justify-center">
-            <div className="w-4 h-4 rounded-full bg-ts animate-ping"></div> 
-           </div> 
-          </button> 
+        <div>
+          <button priority={true} ref={navUt}>
+            <div className="w-8 h-8 rounded-full bg-ts flex items-center justify-center">
+              <div className="w-4 h-4 rounded-tl-full rounded-tr-full -rotate-[130deg] rounded-br-full from-bg-tp to-bg-ts bg-gradient-to-b "></div>
+            </div>
+          </button>
         </div>
       </nav>
     </>
