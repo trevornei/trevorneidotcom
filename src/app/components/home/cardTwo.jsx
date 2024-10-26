3fcd686c79eb4164ed5a09772f627bc1378bd662
trevornei <117675657+trevornei@users.noreply.github.com>
2024-10-26 16:00:36 -0600

    Fixed x axis overflow issue

--- src/app/components/home/cardTwo.jsx
+++ src/app/components/home/cardTwo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function CardTwo() {
     return (
         <>
             <Image
-                className="absolute inset-0 -z-20 blur-[200px] -rotate-55 opacity-100" // Added opacity to make it subtle
+                className="absolute overflow-x-hidden inset-0 -z-20 blur-[200px] -rotate-55 " // Added opacity to make it subtle
                 src="/assets/images/ma_cherry/image_8.png"
                 alt="Decorative Background"
                 width={3000}
@@ -17,28 +18,32 @@ export default function CardTwo() {
 
                 {/* Foreground Icons */}
                 <div className="flex xl:space-x-24">
-                    <Image
-                        src="/assets/images/github-mark/github-mark-white.png"
-                        alt="Github logo"
-                        width={100}
-                        height={100}
-                    />
-                    <Image
-                        src="/assets/images/linkedin-logos/LI-In-Bug.png"
-                        alt="LinkedIn logo"
-                        width={100}
-                        height={100}
-                    />
-                    <Image
-                        src="/assets/images/x-logo/logo-white.png"
-                        alt="Another logo"
-                        width={100}
-                        height={100}
-                    />
+                    <a href="https://www.github.com/trevornei" target="_blank" rel="noopener noreferrer">
+                        <Image
+                            src="/assets/images/github-mark/github-mark-white.png"
+                            alt="Github logo"
+                            width={100}
+                            height={100}
+                        />
+                    </a>
+                    <a href="https://www.linkedin.com/in/trevornei-dev/" target="_blank" rel="noopener noreferrer" >
+                        <Image
+                            src="/assets/images/linkedin-logos/LI-In-Bug.png"
+                            alt="LinkedIn logo"
+                            width={100}
+                            height={100}
+                        />
+                    </a>
+                    <a href="https://x.com/trevv_dev" target="_blank" rel="noopener noreferrer">
+                        <Image
+                            src="/assets/images/x-logo/logo-white.png"
+                            alt="Another logo"
+                            width={100}
+                            height={100}
+                        />
+                    </a>
                 </div>
-                
             </div>
-
         </>
     )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <head /> 
-      <body>
+      <body className="overflow-x-hidden">
         {children}
       </body>
     </html>
