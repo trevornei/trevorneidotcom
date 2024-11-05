3917a1af64a82b9e8732794e9558972e0d6978f0
trevornei <117675657+trevornei@users.noreply.github.com>
2024-11-05 11:37:54 -0700

    Added .npmrc to the git ignore file

--- src/app/components/home/hero.jsx
+++ src/app/components/home/hero.jsx
@@ -8,9 +8,9 @@ export default function Hero() {
           Trevor Nei
         </h1>
       </div>
-      <div className="b-36 relative mb-36 mt-28 flex h-1/2 w-10/12 flex-col items-center justify-end overflow-hidden rounded-3xl border-2 border-tpink sm:h-[300px] sm:w-10/12 sm:flex-row md:h-auto md:w-10/12 md:flex-row lg:h-[500px] lg:w-10/12 xl:h-[500px] xl:w-10/12 xl:flex-row 2xl:w-10/12">
+      <div className="b-36 relative mb-36 mt-28 flex h-1/2 w-10/12 flex-col items-center justify-end overflow-hidden rounded-3xl border-2 border-tpink/20 shadow-lg shadow-tp/30 sm:h-[300px] sm:w-10/12 sm:flex-row md:h-auto md:w-10/12 md:flex-row lg:h-[500px] lg:w-10/12 xl:h-[500px] xl:w-10/12 xl:flex-row 2xl:w-10/12">
         {/* PROFILE EXTENDING THE LENGTH OF THE SCREEN. */}
-        <div className="sm:border-b-none relative -z-0 flex h-full w-full flex-row items-start justify-center overflow-hidden border-b-2 border-tpink backdrop-blur-md sm:-z-10 sm:h-full sm:w-[600px] sm:border-b-0 md:absolute md:left-0 md:-z-10 md:h-full md:w-full md:items-start md:justify-start md:rounded-r-none md:border-b-0 lg:absolute lg:left-0 lg:-z-10 lg:w-full lg:flex-row lg:items-start lg:justify-start lg:border-b-0 xl:absolute xl:h-full xl:w-full xl:border-b-0 2xl:w-full 2xl:border-b-0">
+        <div className="sm:border-b-none relative -z-0 flex h-full w-full flex-row items-start justify-center overflow-hidden border-b-2 border-tpink/20 backdrop-blur-md sm:-z-10 sm:h-full sm:w-[600px] sm:border-b-0 md:absolute md:left-0 md:-z-10 md:h-full md:w-full md:items-start md:justify-start md:rounded-r-none md:border-b-0 lg:absolute lg:left-0 lg:-z-10 lg:w-full lg:flex-row lg:items-start lg:justify-start lg:border-b-0 xl:absolute xl:h-full xl:w-full xl:border-b-0 2xl:w-full 2xl:border-b-0">
           <Image
             src="/assets/images/ma_cherry/image_2.png"
             className="absolute inset-0 -z-10 animate-rotateAndScale object-cover blur-2xl"
@@ -21,12 +21,12 @@ export default function Hero() {
           <Image
             src="/assets/images/profile_pictures/profile_picture.png"
             className="relative z-10 translate-y-6 border-r-0 grayscale md:h-full md:w-auto"
-            alt="Shoshone"
+            alt="A png illustration that aids in the visual representation of the website."
             width={500}
             height={500}
           />
         </div>
-        <div className="relative z-10 my-0 flex h-full w-full flex-col items-center justify-end overflow-hidden rounded-3xl rounded-l-none bg-black/50 sm:h-[300px] sm:w-full md:w-1/2 md:items-end md:rounded-l-none lg:h-full lg:w-96 lg:items-center lg:justify-center lg:rounded-l-none xl:w-1/2 xl:items-end xl:justify-center xl:rounded-l-none 2xl:w-1/2 2xl:rounded-l-none">
+        <div className="relative z-10 my-0 flex h-full w-full flex-col items-center justify-end overflow-hidden rounded-t-none bg-black/50 sm:h-[300px] sm:w-full md:w-1/2 md:items-end md:rounded-3xl md:rounded-l-none lg:h-full lg:w-96 lg:items-center lg:justify-center lg:rounded-l-none xl:w-1/2 xl:items-end xl:justify-center xl:rounded-l-none 2xl:w-1/2 2xl:rounded-l-none">
           <div className="m-0 my-0 flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-3xl bg-black/50 p-10 shadow-lg backdrop-blur-md sm:m-0 sm:my-0 sm:h-full sm:items-center sm:rounded-none sm:py-10 md:w-full md:rounded-l-none md:py-10 lg:m-0 lg:h-full lg:w-full lg:items-center lg:justify-center lg:rounded-l-none lg:px-8 lg:py-10 xl:h-full xl:w-full xl:items-center xl:justify-center xl:rounded-l-none xl:py-16 2xl:rounded-l-none">
             <Image
               src="/assets/images/ma_cherry/el 6.png"
