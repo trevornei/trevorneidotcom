32676575901efe59528bd8eb25461dd875cb9c36
trevornei <117675657+trevornei@users.noreply.github.com>
2024-11-05 13:26:50 -0700

    Added animation to the website.

--- src/app/components/home/hero.jsx
+++ src/app/components/home/hero.jsx
@@ -1,6 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
 import Image from "next/image";
+import gsap from "gsap";
+import ScrambleTextPlugin from "gsap/ScrambleTextPlugin";
+
+gsap.registerPlugin(ScrambleTextPlugin);
 
 export default function Hero() {
+  useEffect(() => {
+    gsap.to(".software", {
+      revealDelay: 2,
+      duration: 2,
+      scrambleText: "SOFTWARE",
+    }),
+      gsap.to(".engineer", {
+        revealDelay: 2.52,
+        duration: 2,
+        scrambleText: "ENGINEER",
+      }),
+      gsap.fromTo(
+        ".moveHero",
+        { opacity: 0, x: -300 },
+        { opacity: 1, x: 0, duration: 1.75 },
+      );
+  }, []);
+
   return (
     <>
       <div className="absolute z-30 mb-20 hidden -rotate-[10deg] overflow-hidden bg-tp sm:left-[215px] sm:top-[497px] sm:block md:left-[200px] md:top-[500px] md:mb-32 md:block lg:left-[400px] lg:top-[700px] lg:block xl:block 2xl:block">
@@ -8,7 +33,7 @@ export default function Hero() {
           Trevor Nei
         </h1>
       </div>
-      <div className="b-36 relative mb-36 mt-28 flex h-1/2 w-10/12 flex-col items-center justify-end overflow-hidden rounded-3xl border-2 border-tpink/20 shadow-lg shadow-tp/30 sm:h-[300px] sm:w-10/12 sm:flex-row md:h-auto md:w-10/12 md:flex-row lg:h-[500px] lg:w-10/12 xl:h-[500px] xl:w-10/12 xl:flex-row 2xl:w-10/12">
+      <div className="b-36 moveHero relative mb-36 mt-28 flex h-1/2 w-10/12 flex-col items-center justify-end overflow-hidden rounded-3xl border-2 border-tpink/20 shadow-lg shadow-tp/30 sm:h-[300px] sm:w-10/12 sm:flex-row md:h-auto md:w-10/12 md:flex-row lg:h-[500px] lg:w-10/12 xl:h-[500px] xl:w-10/12 xl:flex-row 2xl:w-10/12">
         {/* PROFILE EXTENDING THE LENGTH OF THE SCREEN. */}
         <div className="sm:border-b-none relative -z-0 flex h-full w-full flex-row items-start justify-center overflow-hidden border-b-2 border-tpink/20 backdrop-blur-md sm:-z-10 sm:h-full sm:w-[600px] sm:border-b-0 md:absolute md:left-0 md:-z-10 md:h-full md:w-full md:items-start md:justify-start md:rounded-r-none md:border-b-0 lg:absolute lg:left-0 lg:-z-10 lg:w-full lg:flex-row lg:items-start lg:justify-start lg:border-b-0 xl:absolute xl:h-full xl:w-full xl:border-b-0 2xl:w-full 2xl:border-b-0">
           <Image
@@ -35,14 +60,14 @@ export default function Hero() {
               width={600}
               height={600}
             />
-            <h1 className="z-30 font-bold text-tp sm:hidden sm:px-2 sm:py-px sm:text-xl md:hidden md:px-2 md:py-1 md:text-2xl lg:hidden lg:px-4 lg:py-2 lg:text-4xl xl:hidden 2xl:hidden">
+            <h1 className="scrambleName z-30 font-bold text-tp sm:hidden sm:px-2 sm:py-px sm:text-xl md:hidden md:px-2 md:py-1 md:text-2xl lg:hidden lg:px-4 lg:py-2 lg:text-4xl xl:hidden 2xl:hidden">
               Trevor Nei
             </h1>
             <div className="flex flex-row items-start justify-start gap-x-2 sm:flex-col">
-              <h1 className="mx-none text-2xl font-extrabold text-white md:text-5xl lg:text-6xl xl:mx-8 xl:pb-5 xl:text-6xl">
+              <h1 className="mx-none software text-2xl font-extrabold text-white md:text-5xl lg:text-6xl xl:mx-8 xl:pb-5 xl:text-6xl">
                 SOFTWARE
               </h1>
-              <h1 className="text-2xl font-extrabold text-tp md:text-5xl lg:text-6xl xl:mx-8 xl:text-6xl">
+              <h1 className="engineer text-2xl font-extrabold text-tp md:text-5xl lg:text-6xl xl:mx-8 xl:text-6xl">
                 ENGINEER
               </h1>
             </div>
