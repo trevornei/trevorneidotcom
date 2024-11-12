e2d6257bd69a1db39736231dfe2a147fb4269b43
trevornei <117675657+trevornei@users.noreply.github.com>
2024-11-12 14:27:23 -0700

    Added token

--- pages/notes/NotesLayout.jsx
+++ pages/notes/NotesLayout.jsx
@@ -1,6 +1,5 @@
 import Nav from "../../src/app/components/Nav.jsx";
 import Footer from "../../src/app/components/Footer.jsx";
-import "./notes.css";
 
 export default function NotesLayout({ children }) {
   return (
