b6b1424fcdf070ed09cb80ca7e52ff2ca02741a3
trevornei <117675657+trevornei@users.noreply.github.com>
2024-12-05 15:13:26 -0700

    progress

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -2,19 +2,40 @@
 import Nav from "../../src/app/components/Nav.jsx";
 import Footer from "../../src/app/components/Footer.jsx";
 import "./notes.css";
+import Image from "next/image.js";
 
 import blogPosts from "./posts.json";
 export default function Notes() {
   return (
     <>
       <Nav className="" />
+
       <div className="flex flex-col items-center justify-center">
-        <div className="flex w-10/12 flex-col items-center justify-center">
+        <div className="glassy flex w-10/12 flex-col items-start justify-center">
+          <div className="relative flex h-auto w-full items-center justify-center">
+            <Image
+              src="/assets/images/ma_cherry/el 1.png"
+              className="absolute inset-0 object-cover"
+              alt="Shoshone"
+              width={1000}
+              height={1000}
+            />
+          </div>
+
+          <div className="relative mb-20 font-bold">
+            <h1>Welcome.</h1>
+            <h4>I write about technology, culture, and travel.</h4>
+          </div>
+
           {blogPosts.map((posts) => (
-            <div className="flex flex-col gap-2" key={posts.id}>
-              <h1 className="text-green-400">{posts.title}</h1>
+            <div
+              className="glassy-mid relative flex w-full flex-col gap-2 overflow-clip"
+              key={posts.id}
+            >
+              <h1 className="">{posts.title}</h1>
               <p>{posts.description}</p>
               <p>{posts.date}</p>
+              <button className="button">Click Here.</button>
             </div>
           ))}
         </div>
