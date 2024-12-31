05b37299183b13bbae5cd8fefee7afcb03fbae37
trevornei <117675657+trevornei@users.noreply.github.com>
2024-12-30 22:52:05 -0700

    Added photos and text.

--- src/app/home/cardThree.jsx
+++ src/app/home/cardThree.jsx
@@ -1,6 +1,58 @@
 import React from "react";
 import Image from "next/image";
+import meClimb from "../../../public/assets/images/Climbing/climbing.jpeg";
+import pets from "../../../public/assets/images/Climbing/pets.jpeg";
+import action from "../../../public/assets/images/Climbing/action.jpeg";
+import rafting from "../../../public/assets/images/Climbing/rafting.jpeg";
+import wr from "../../../public/assets/images/Climbing/wr.jpeg";
 
 export default function CardThree() {
-  return <></>;
+  return (
+    <>
+      <div className="relative xl:h-[1000px] xl:w-full">
+        <div className="absolute z-10 flex-col items-center justify-center xl:left-[500px]">
+          <h1 className="text-tp">Missoula MT</h1>
+          <h2 className="">University of Montana</h2>
+          <h3 className="">Studying Computer Science</h3>
+        </div>
+        <div className="relative h-[300px] w-[300px] bg-transparent">
+          <Image
+            src={meClimb}
+            alt="Climbing"
+            className="absolute -z-20 origin-top-left rotate-12 xl:left-[950px] xl:top-[200px]"
+            width={300}
+            height={300}
+          />
+          <Image
+            src={action}
+            alt="Action"
+            className="absolute inset-0 -z-10 origin-top-left rotate-12 xl:left-[200px] xl:top-[300px]"
+            width={300}
+            height={300}
+          />
+          <Image
+            src={pets}
+            alt="Pets"
+            className="absolute inset-0 top-0 z-0 origin-top-left -rotate-12 xl:left-[1100px]"
+            width={300}
+            height={300}
+          />
+          <Image
+            src={rafting}
+            alt="Rafting"
+            className="absolute inset-0 z-10 xl:-top-[300px] xl:left-[500px]"
+            width={300}
+            height={300}
+          />
+          <Image
+            src={wr}
+            alt="WR"
+            className="absolute inset-0 z-20 origin-top-left -rotate-[5deg]"
+            width={300}
+            height={300}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
