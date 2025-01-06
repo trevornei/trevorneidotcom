bb76a21360c6d484b9ca0a3f1fb1cd3e06dbead7
trevornei <117675657+trevornei@users.noreply.github.com>
2025-01-06 14:27:48 -0700

    Got rid of images

--- src/app/home/cardThree.jsx
+++ src/app/home/cardThree.jsx
@@ -11,50 +11,15 @@ export default function CardThree() {
     <>
       <div className="relative xl:h-[1000px] xl:w-full">
         <div className="flex flex-col items-center justify-center">
-          <span className="badge indicator-item badge-secondary">SYN</span>
           <div className="glassy indicator flex-col items-start justify-center rounded-md border-[1px] border-white/20 xl:p-4">
-            <h1 className="text-tp">Missoula MT📍</h1>
+            <span className="indicator-item rotate-12 rounded-full text-3xl">
+              📍
+            </span>
+            <h1 className="text-tp">Missoula MT</h1>
             <h2 className="">University of Montana</h2>
             <h3 className="">Studying Computer Science</h3>
           </div>
         </div>
-        {/* <div className="relative h-[300px] w-[300px] bg-transparent">
-          <Image
-            src={meClimb}
-            alt="Climbing"
-            className="absolute -z-20 origin-top-left rotate-12 xl:left-[950px] xl:top-[200px]"
-            width={300}
-            height={300}
-          />
-          <Image
-            src={action}
-            alt="Action"
-            className="absolute inset-0 -z-10 origin-top-left rotate-12 xl:left-[200px] xl:top-[300px]"
-            width={300}
-            height={300}
-          />
-          <Image
-            src={pets}
-            alt="Pets"
-            className="absolute inset-0 top-0 z-0 origin-top-left -rotate-12 xl:left-[1100px]"
-            width={300}
-            height={300}
-          />
-          <Image
-            src={rafting}
-            alt="Rafting"
-            className="absolute inset-0 z-10 xl:-top-[300px] xl:left-[500px]"
-            width={300}
-            height={300}
-          />
-          <Image
-            src={wr}
-            alt="WR"
-            className="absolute inset-0 z-20 origin-top-left -rotate-[5deg]"
-            width={300}
-            height={300}
-          />
-        </div> */}
       </div>
     </>
   );
