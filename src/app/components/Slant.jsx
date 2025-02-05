c93cbb6396ab4e1d27f08d548256e6986ff8c286
trevornei <117675657+trevornei@users.noreply.github.com>
2025-02-05 14:05:13 -0700

    Changed capitolization for slant to Slant.

--- src/app/components/Slant.jsx
+++ src/app/components/Slant.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-export default function slant() {
+export default function Slant() {
   return (
     <div className="border-2 border-red-700 xl:flex xl:h-[600px] xl:w-[40px] xl:flex-row"></div>
   );
