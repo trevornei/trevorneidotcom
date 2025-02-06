872be2bb30711a0adac6cc36b1d20c77fa3b9cf6
trevornei <117675657+trevornei@users.noreply.github.com>
2025-02-06 09:39:28 -0700

    Changed the size of and shape of divs in the background.

--- src/app/components/Slant.jsx
+++ src/app/components/Slant.jsx
@@ -2,6 +2,6 @@
 
 export default function Slant() {
   return (
-    <div className="border-2 border-red-700 xl:flex xl:h-[600px] xl:w-[40px] xl:flex-row"></div>
+    <div className="border-l-[1px] border-red-700 xl:flex xl:h-[600px] xl:w-[40px] xl:flex-row"></div>
   );
 }
