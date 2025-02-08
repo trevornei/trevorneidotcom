934a917b9ece464109ce8eab46cc8e3bceace780
trevornei <117675657+trevornei@users.noreply.github.com>
2025-02-07 21:44:13 -0700

    Added star snowflakes

--- src/app/components/Slants.jsx
+++ src/app/components/Slants.jsx
@@ -9,7 +9,7 @@ export default function Slants() {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    const numberOfSlants = 40;
+    const numberOfSlants = 1500;
     const componentsArrary = [];
 
     for (let i = 0; i < numberOfSlants; i++) {
@@ -23,24 +23,25 @@ export default function Slants() {
     if (containerRef.current) {
       const children = containerRef.current.children;
       Array.from(children).forEach((child) => {
-        const max = 400;
+        let color = ["tp"];
+        // let randomColor = Math.floor(Math.random() * color.length);
+
+        // child.classList.add(`${color[randomColor]}`);
+        child.classList.add("bg-teal-300");
+
+        const max = 1800;
         const random = Math.floor(Math.random() * max);
-        gsap.to(child, { y: random, duration: 0.75 });
+        gsap.to(child, { y: random, duration: 10, stagger: 150, delay: 0 });
       });
     }
   }, [slantComponents]);
-
-  useGSAP(() => {
-    // Instead of iterating an undefined variable or a number, you should target the actual DOM elements.
-    // One way is to select the children of the container:
-  }, []);
 
   return (
     <>
       {/* Parent Div*/}
       <div
         ref={containerRef}
-        className="absolute right-0 -z-10 block -rotate-35 xl:m-0 xl:flex xl:flex-row"
+        className="absolute inset-x-0 -top-[100px] -z-10 flex rotate-12 flex-wrap xl:m-0 xl:flex xl:h-auto xl:w-[2900px] xl:flex-row"
         id="slantPop"
       >
         {/* Each Child Div's */}
