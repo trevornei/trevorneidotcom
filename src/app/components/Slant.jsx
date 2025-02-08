af21e5703a9433ab1bbce9010e4e9b8e46a4b300
trevornei <117675657+trevornei@users.noreply.github.com>
2025-02-07 21:44:13 -0700

    Added star snowflakes

--- src/app/components/Slant.jsx
+++ src/app/components/Slant.jsx
@@ -2,6 +2,6 @@
 
 export default function Slant() {
   return (
-    <div className="border-l-[50px] border-tp xl:flex xl:h-[600px] xl:w-[40px] xl:flex-row"></div>
+    <div className="sm:h-300 sm:flex sm:w-auto xl:mx-[5px] xl:flex xl:h-[1px] xl:w-[1px] xl:flex-row"></div>
   );
 }
