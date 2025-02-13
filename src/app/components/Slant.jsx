3e6ea646f858b362731c9122e63182bd48ae4090
trevornei <117675657+trevornei@users.noreply.github.com>
2025-02-13 16:38:29 -0700

    Changed the width and height of the slants componenet being rendered at the xl breakpoint

--- src/app/components/Slant.jsx
+++ src/app/components/Slant.jsx
@@ -2,6 +2,6 @@
 
 export default function Slant() {
   return (
-    <div className="flex-rows flex h-px w-px sm:h-px sm:w-px md:h-px md:w-px lg:h-px lg:w-px xl:mx-[5px] xl:flex xl:h-[1px] xl:w-[1px] xl:flex-row"></div>
+    <div className="flex-rows xl: flex h-px w-px sm:h-px sm:w-px md:h-px md:w-px lg:h-px lg:w-px xl:mx-[5px] xl:flex xl:h-[30px] xl:w-[1px] xl:flex-row"></div>
   );
 }
