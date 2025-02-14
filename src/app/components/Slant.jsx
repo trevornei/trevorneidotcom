40c6317ad813d9dadebed30704b6396d7086ba01
trevornei <117675657+trevornei@users.noreply.github.com>
2025-02-13 20:06:58 -0700

    Changed the shape and the colors of the slants

--- src/app/components/Slant.jsx
+++ src/app/components/Slant.jsx
@@ -2,6 +2,6 @@
 
 export default function Slant() {
   return (
-    <div className="flex-rows xl: flex h-px w-px sm:h-px sm:w-px md:h-px md:w-px lg:h-px lg:w-px xl:mx-[5px] xl:flex xl:h-[30px] xl:w-[1px] xl:flex-row"></div>
+    <div className="flex-rows flex h-[150px] w-[30px] sm:h-[150px] sm:w-[30px] md:h-[150px] md:w-[30px] lg:h-[150px] lg:w-[30px] xl:mx-[5px] xl:flex xl:h-[150px] xl:w-[30px] xl:flex-row"></div>
   );
 }
