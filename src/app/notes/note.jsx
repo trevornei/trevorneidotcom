50b20c45f6ca60ca0e2ccb2d01ca2a8eb0225e93
trevornei <117675657+trevornei@users.noreply.github.com>
2025-04-03 12:31:48 -0600

    There was an empty async call that was fucking up prod...

--- src/app/notes/note.jsx
+++ src/app/notes/note.jsx
@@ -3,33 +3,6 @@ import { db, doc, getDoc } from '../../../firebase';
 
 
 export default function note () {
-  // States: postData, Loading, error.
-  const [postData, setPostData] useState(null)	
-  const [loading, setLoading] useState(True)	
-  const [error, setError] useState(null)
-
-  useEffect(() => {
-	async function getPost() {
-		try {
-		const docRef = doc(db, dd)
-		const docSanp = await getDoc(docRef)
-
-			if (docSnap.exists()) {
-          			setPostData(docSnap.data());
-        		} else {
-          			setError("No such document!");
-        		}
-		} catch (e) {
-			setError("Error getting my document", e)
-		} finally {
-			setLoading(false)
-		}
-	}
-	getPost() 
-  })
 	<>
-		<div className="z-50 w-3/4 sm:w-3/4 md:w-3/4 ">
-			
-		</div>
 	</>
 }
