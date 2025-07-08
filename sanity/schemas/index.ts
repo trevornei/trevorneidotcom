77ba3b036bfe60f11d891e0dc81d4ef1ece4e19d
trevornei <117675657+trevornei@users.noreply.github.com>
2025-07-08 15:27:01 -0600

    Catching up commits, most of the the way through implemnting the sanity project. Just adde dthe project schema.

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,11 @@
+// This file is for organization...
+// ---> When we have a new schema file, we add it here and our 
+// ---> sanity.config.ts file points here...
+
+
+import project from 'project-schema';
+
+const schemas = [project];
+
+export default schemas;
+
---
+++ sanity/schemas/project-schema.ts
@@ -0,0 +1,45 @@
+const project = {
+  name: 'note',
+  title: 'Notes',
+  type: 'document',
+  fields: [
+    {
+      name: 'name', 
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'name'},
+    },
+    {
+      name: 'image',
+      title: 'image',
+      type: 'image',
+      options: {hotspot: true},
+      fields: [
+        {
+          name: 'alt', 
+          title: 'Alt',
+          type: 'string', 
+        },
+      ],
+    },
+    {
+      name: 'url',
+      title: 'Url', 
+      type: 'url',
+    },
+    {
+      name: 'content',
+      title: 'Content', 
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+  ],
+};
+
+
+export default project;
