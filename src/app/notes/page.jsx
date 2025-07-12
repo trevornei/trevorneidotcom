288f8b4b57359aaf38a5b33484c936ceeb93b8e0
trevornei <117675657+trevornei@users.noreply.github.com>
2025-07-12 09:39:29 -0600

    Added react suspense component to the src/app/notes/page.jsx

--- src/app/notes/page.jsx
+++ src/app/notes/page.jsx
@@ -3,6 +3,8 @@ import { getProjects, urlFor } from '../../../sanity/sanity-utils';
 import Image from 'next/image';
 import { notFound } from "next/navigation";
 import Link from "next/link";
+import bgImg from '../../../public/assets/images/ma_cherry/image_2.png';
+import { Suspense } from 'react';
 
 export default async function page() {
   const projects = await getProjects();
@@ -10,18 +12,25 @@ export default async function page() {
     return (
       <p className="text-4xl text-white">No data was found</p>
     ) 
-    if (projects) {
-      return (
-          <h1 className="text-4xl font-bold text-red-500">${projects}</h1>)
-    }
   };
   return (
     <>
       <Nav />
-
-        {projects.map((project) => {
+      <div>
+        <Image 
+          src={bgImg} 
+          alt='background blured image.'
+          width={750}
+          height={750}
+          objectFit="cover"
+          className="absolute inset-0 -z-10 blur-[200px]"
+        />
+      </div>
+      <div className="m-6 md:m-8 lg:m-10 flex flex-col">  
+      <Suspense fallback={<Loading />}>
+      {projects.map((project) => {
           return (
-          <div key={project._id} className="text-4xl text-white border-2 border-white/50 p-8"> 
+          <div key={project._id} className="bg-neutral-400/20 hover:bg-neutral-400/30 text-2xl md:text-3xl lg:text-4xl xl:text-l text-white border-[.5px] border-tp rounded-xl rounded-r-3xl overflow-clip p-8 m-4 lg:m-6 xl:m-10"> 
                 {project.image && (
                   <Image 
                     src={project.image} 
@@ -29,13 +38,25 @@ export default async function page() {
                     width={250}
                     height={250}
                     objectFit="cover"
+                    className="rounded-2xl"
                   />
                 )}
             {project.name}
           </div>
+          )
 
-          ) 
         })}
+
+    </Suspense>
+    </div>
     </>
   )
+}
+
+function Loading () {
+  <>
+    return <div className="bg-neutral-20 w-[100px] h-[25px] md:w-[200px] md:h-[50px]  lg:w-[400px] lg:h-[100px]">
+             <p className="text-4xl">... LOADING ...</p>
+           </div>
+  </>
 }
