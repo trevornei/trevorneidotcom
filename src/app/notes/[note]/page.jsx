5c6664e0bf6c3f772746d53bea90803c3a413780
trevornei <117675657+trevornei@users.noreply.github.com>
2025-07-13 07:19:16 -0600

    Added the Nav component to each individual note.

--- src/app/notes/[note]/page.jsx
+++ src/app/notes/[note]/page.jsx
@@ -1,11 +1,15 @@
 import { getProject, urlFor } from '../../../../sanity/sanity-utils';
 import Image from 'next/image';
 import { PortableText } from '@portabletext/react';
-import bg from '../../../../public/bg.png'
+import bg from '../../../../public/bg.png';
+import Nav from '../../components/Nav';
+
 export default async function note({ params }) {
   const slug = params.note
   const note = await getProject(slug) 
   return (
+    <>
+    <Nav /> 
     <div className="mx-10 items-center content-start flex flex-col content-evenly gap-8 my-10 bg-neutral-950/10 w-auto h-full" key={note._id}>
       <Image
         src={bg}
@@ -30,5 +34,5 @@ export default async function note({ params }) {
         <PortableText value={note.content} />    
       </div>
     </div> 
-  )
+  </>)
 }
