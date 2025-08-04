40e83d2332f733cee8f6154d4304f443379802c3
trevornei <117675657+trevornei@users.noreply.github.com>
2025-08-03 22:40:55 -0600

    Added a 3D object to my websitegit add .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,21 @@
 import Nav from "./components/Nav";
 import CardTwo from "./home/cardTwo";
 import Hero from "./home/hero";
-import Slants from "./components/Slants.jsx";
 import CardThree from "./home/cardThree";
+import Spline from '@splinetool/react-spline/next';
+
 
 export default function Home() {
   return (
     <>
       <div className="relative overflow-hidden">
         <Nav className="fixed right-0 top-0 z-50" />
-        <Slants className="fixed right-96 -rotate-55" />
         <div className="flex flex-col items-center justify-center overflow-hidden">
           <Hero />
+          <Spline
+           className="absolute inset-0 md:-top-24 -z-20"
+           scene="https://prod.spline.design/cV2-clwFjWSOxbGh/scene.splinecode" 
+          /> 
           <CardTwo />
        	  <CardThree />
 	</div>
