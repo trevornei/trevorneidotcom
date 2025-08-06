93910e301354e1237b78d32419d9716b312776d6
trevornei <117675657+trevornei@users.noreply.github.com>
2025-08-06 12:14:09 -0600

    Added additional text to the landing page of <domain>/notes

--- src/app/notes/page.jsx
+++ src/app/notes/page.jsx
@@ -16,6 +16,10 @@ export default async function page() {
     <>
       <Nav />
       <div className="m-6 md:m-8 lg:m-10 flex flex-col">  
+      <div className="text-white text-4xl">
+        <h1 className="text-tp">ALL CAPS</h1>
+        <h3>Trying to find Bobby Tables -__-</h3>
+      </div>
       {projects.map((project) => {
           return (
           <div key={project._id} className="bg-transparent hover:bg-sky-400/10 text-2xl md:text-3xl lg:text-4xl xl:text-l text-white border-[1px] border-tp/20 rounded-md overflow-clip p-8 m-4 lg:m-6 xl:m-10"> 
